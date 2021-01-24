b72ad17fa709294fa2e7e46a1559323223e5faee
RuslanKoval <ruslan.koval21@gmail.com>
2021-01-24 23:15:15 +0300

    first commit

---
+++ src/TestClass.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace rus\testPackage;
+
+/**
+ * Class TestClass
+ * @package rus\testPackage
+ */
+class TestClass
+{
+    /**
+     * @return string
+     */
+    public function run()
+    {
+        return 'Hello world';
+    }
+}
